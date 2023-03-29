49ad8aafd51b8c3f95b0c17b39fc49d937249e7c
kibum <kibum@kibumui-noteubug.local>
2023-03-29 12:25:19 +0900

    Deckie's file

---
+++ ContentView.swift
@@ -0,0 +1,88 @@
+import SwiftUI
+import AVFoundation
+import AVKit
+
+
+struct PlayerView: UIViewRepresentable {
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
+    }
+
+    func makeUIView(context: Context) -> UIView {
+        return LoopingPlayerUIView(frame: .zero)
+    }
+}
+
+class LoopingPlayerUIView: UIView {
+    private let playerLayer = AVPlayerLayer()
+    private var playerLooper: AVPlayerLooper?
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        let fileUrl = Bundle.main.url(forResource: "VideoWithBlock", withExtension: "mov")!
+        let asset = AVAsset(url: fileUrl)
+        let item = AVPlayerItem(asset: asset)
+
+        let player = AVQueuePlayer()
+        playerLayer.player = player
+        playerLayer.videoGravity = .resizeAspectFill
+        layer.addSublayer(playerLayer)
+
+        playerLooper = AVPlayerLooper(player: player, templateItem: item)
+
+        player.play()
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer.frame = bounds
+    }
+}
+
+struct ContentView: View {
+
+    @State var isLoading: Bool = true
+    private let timer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
+
+    @State var animationTick = false
+    
+    var body: some View {
+        
+        
+        NavigationView {
+            GeometryReader { geo in
+                ZStack {
+                    PlayerView()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: geo.size.width, height: geo.size.height+100)
+                        .overlay(Color.black.opacity(0.2))
+                        .blur(radius: 1)
+                        .edgesIgnoringSafeArea(.all)
+                    
+                    VStack {
+
+                        
+                        Image("logo")
+                            .resizable()
+                            .frame(width: 600, height: 300)
+                            .multilineTextAlignment(.center)
+                        
+                        Spacer().frame(height: 50.0)
+                        
+                        NavigationLink(destination: StatsScreenView()){
+                            Image("logo")
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                                .multilineTextAlignment(.center)
+                            
+                        }
+                        
+                    }
+                }
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+
+    }
+}
---
+++ Fourth.swift
@@ -0,0 +1,63 @@
+import SwiftUI
+import Foundation
+
+struct FourthScreenView: View {
+    
+    @State private var fontSize = 32.0
+    
+    var body: some View{
+        
+        if #available(iOS 16.0, *) {
+            
+            
+            NavigationStack {
+                VStack {
+                    
+                    
+                    
+                    VStack{
+                        
+                        Text("""
+                            첫 발표에서는 각자가 가장 잘 하는 것을 맡았다면
+                            이번에는 가장 못하는 부분을 서로에게서 배워보자.
+                            """)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                        
+                        Spacer().frame(height: 50.0)
+                        
+                        Text("""
+                            그렇게 우리의 두 번째 챌린지 주제는
+                            무수리들의 공주되기 프로젝트로 정해졌다.
+                            """)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                        .animatableSystemFont(size: fontSize)
+                        .onTapGesture {
+                            withAnimation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 1).repeatForever()) {
+                                fontSize = 72
+                            }
+                        }
+                        
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: FourthScreenView()){
+                        Text("넘어가기 버튼")
+                            .foregroundColor(Color.black)
+                            .frame(width: 100, height: 60, alignment: .center)
+                            .background(RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.gray))
+                    }
+                }
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+}
+
+
